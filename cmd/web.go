20693ce80d287ff2f04cd2401c2b895e0d6e54d9
Meaglith Ma <genedna@gmail.com>
2014-06-24 05:05:43 -0400

    Add listen address/port support

--- cmd/web.go
+++ cmd/web.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+  "fmt"
+  "strconv"
+
   "github.com/astaxie/beego"
   "github.com/codegangsta/cli"
   "github.com/dockboard/docker-registry/models"
@@ -12,10 +15,29 @@ var CmdWeb = cli.Command{
   Usage:       "Start Docker Registry Web Server",
   Description: "Docker web service provide Docker Registry API service and web view for search & comment",
   Action:      runRegistry,
-  Flags:       []cli.Flag{},
+  Flags: []cli.Flag{
+    cli.StringFlag{"address", "", "docker registry listen ip"},
+    cli.IntFlag{"port", 9911, "docker registry listen port"},
+    cli.StringFlag{"qiniu_access", "", "Qiniu.com's access key for backup docker image layer file."},
+    cli.StringFlag{"qiniu_secret", "", "Qiniu.com's secret key for backup docker image layer file."},
+  },
 }
 
-func runRegistry(*cli.Context) {
+func runRegistry(c *cli.Context) {
+  var address, port string
+
+  beego.Trace("[Address] " + strconv.Itoa(len(c.String("address"))))
+  beego.Trace("[Port] " + strconv.Itoa(c.Int("port")))
+
+  //检查 address / port 的合法性
+  if len(c.String("address")) > 0 {
+    address = c.String("address")
+  }
+
+  if len(c.String("port")) > 0 {
+    port = strconv.Itoa(c.Int("port"))
+  }
+
   models.InitDb()
-  beego.Run()
+  beego.Run(fmt.Sprintf("%v:%v", address, port))
 }
