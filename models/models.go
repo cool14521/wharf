05c49a01f658bfd254c0258d081f91705377b68d
Meaglith Ma <genedna@gmail.com>
2014-06-25 15:01:11 +0800

    Add history model.

--- models/models.go
+++ models/models.go
@@ -1,43 +1,43 @@
 package models
 
 import (
-  "fmt"
-  "github.com/astaxie/beego"
-  _ "github.com/go-sql-driver/mysql"
-  "github.com/go-xorm/xorm"
-  "log"
+	"fmt"
+	"github.com/astaxie/beego"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
+	"log"
 )
 
 var Engine *xorm.Engine
 
 func setEngine() {
-  host := beego.AppConfig.String("mysql::Host")
-  name := beego.AppConfig.String("mysql::Name")
-  user := beego.AppConfig.String("mysql::User")
-  passwd := beego.AppConfig.String("mysql::Passwd")
+	host := beego.AppConfig.String("mysql::Host")
+	name := beego.AppConfig.String("mysql::Name")
+	user := beego.AppConfig.String("mysql::User")
+	passwd := beego.AppConfig.String("mysql::Passwd")
 
-  var err error
-  conn := fmt.Sprintf("%v:%v@tcp(%v)/%v?charset=utf8", user, passwd, host, name)
-  beego.Trace("Initialized database connStr ->", conn)
+	var err error
+	conn := fmt.Sprintf("%v:%v@tcp(%v)/%v?charset=utf8", user, passwd, host, name)
+	beego.Trace("Initialized database connStr ->", conn)
 
-  Engine, err = xorm.NewEngine("mysql", conn)
-  if err != nil {
-    log.Fatalf("models.init -> fail to conntect database: %v", err)
-  }
+	Engine, err = xorm.NewEngine("mysql", conn)
+	if err != nil {
+		log.Fatalf("models.init -> fail to conntect database: %v", err)
+	}
 
-  Engine.ShowDebug = true
-  Engine.ShowErr = true
-  Engine.ShowSQL = true
+	Engine.ShowDebug = true
+	Engine.ShowErr = true
+	Engine.ShowSQL = true
 
-  beego.Trace("Initialized database ->", name)
+	beego.Trace("Initialized database ->", name)
 
 }
 
 // InitDb initializes the database.
 func InitDb() {
-  setEngine()
-  err := Engine.Sync(new(User), new(Profile), new(Organization), new(Member), new(Image), new(Repository), new(Tag), new(Comment), new(Star))
-  if err != nil {
-    log.Fatalf("models.init -> fail to sync database: %v", err)
-  }
+	setEngine()
+	err := Engine.Sync(new(User), new(Profile), new(Organization), new(Member), new(Image), new(Repository), new(Tag), new(Comment), new(Star), new(History))
+	if err != nil {
+		log.Fatalf("models.init -> fail to sync database: %v", err)
+	}
 }
