5dfbc04ac1d5b0f00f134befb2989341404f0ae1
Meaglith Ma <genedna@gmail.com>
2014-07-03 01:36:39 -0400

    Add semantic 0.18.0 package files

--- routers/router.go
+++ routers/router.go
@@ -1,41 +1,41 @@
 package routers
 
 import (
-  "github.com/astaxie/beego"
-  "github.com/dockboard/docker-registry/controllers"
+	"github.com/astaxie/beego"
+	"github.com/dockboard/docker-registry/controllers"
 )
 
 func init() {
-  beego.Router("/", &controllers.MainController{})
-  beego.Router("/_status", &controllers.StatusController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/_status", &controllers.StatusController{})
 
-  beego.Router("/_ping", &controllers.PingController{}, "get:GetPing")
+	beego.Router("/_ping", &controllers.PingController{}, "get:GetPing")
 
-  ns := beego.NewNamespace("/v1",
-    beego.NSRouter("/_ping", &controllers.PingController{}, "get:GetPing"),
+	ns := beego.NewNamespace("/v1",
+		beego.NSRouter("/_ping", &controllers.PingController{}, "get:GetPing"),
 
-    beego.NSRouter("/_status", &controllers.StatusController{}),
+		beego.NSRouter("/_status", &controllers.StatusController{}),
 
-    beego.NSRouter("/users", &controllers.UsersController{}, "get:GetUsers"),
-    beego.NSRouter("/users", &controllers.UsersController{}, "post:PostUsers"),
+		beego.NSRouter("/users", &controllers.UsersController{}, "get:GetUsers"),
+		beego.NSRouter("/users", &controllers.UsersController{}, "post:PostUsers"),
 
-    beego.NSNamespace("/repositories",
-      beego.NSRouter("/:namespace/:repo_name/tags/:tag", &controllers.RepositoryController{}, "put:PutTag"),
-      beego.NSRouter("/:namespace/:repo_name/images", &controllers.RepositoryController{}, "put:PutRepositoryImages"),
-      beego.NSRouter("/:namespace/:repo_name/images", &controllers.RepositoryController{}, "get:GetRepositoryImages"),
-      beego.NSRouter("/:namespace/:repo_name/tags", &controllers.RepositoryController{}, "get:GetRepositoryTags"),
-      beego.NSRouter("/:namespace/:repo_name", &controllers.RepositoryController{}, "put:PutRepository"),
-    ),
+		beego.NSNamespace("/repositories",
+			beego.NSRouter("/:namespace/:repo_name/tags/:tag", &controllers.RepositoryController{}, "put:PutTag"),
+			beego.NSRouter("/:namespace/:repo_name/images", &controllers.RepositoryController{}, "put:PutRepositoryImages"),
+			beego.NSRouter("/:namespace/:repo_name/images", &controllers.RepositoryController{}, "get:GetRepositoryImages"),
+			beego.NSRouter("/:namespace/:repo_name/tags", &controllers.RepositoryController{}, "get:GetRepositoryTags"),
+			beego.NSRouter("/:namespace/:repo_name", &controllers.RepositoryController{}, "put:PutRepository"),
+		),
 
-    beego.NSNamespace("/images",
-      beego.NSRouter("/:image_id/ancestry", &controllers.ImageController{}, "get:GetImageAncestry"),
-      beego.NSRouter("/:image_id/json", &controllers.ImageController{}, "get:GetImageJSON"),
-      beego.NSRouter("/:image_id/layer", &controllers.ImageController{}, "get:GetImageLayer"),
-      beego.NSRouter("/:image_id/json", &controllers.ImageController{}, "put:PutImageJson"),
-      beego.NSRouter("/:image_id/layer", &controllers.ImageController{}, "put:PutImageLayer"),
-      beego.NSRouter("/:image_id/checksum", &controllers.ImageController{}, "put:PutChecksum"),
-    ),
-  )
+		beego.NSNamespace("/images",
+			beego.NSRouter("/:image_id/ancestry", &controllers.ImageController{}, "get:GetImageAncestry"),
+			beego.NSRouter("/:image_id/json", &controllers.ImageController{}, "get:GetImageJSON"),
+			beego.NSRouter("/:image_id/layer", &controllers.ImageController{}, "get:GetImageLayer"),
+			beego.NSRouter("/:image_id/json", &controllers.ImageController{}, "put:PutImageJson"),
+			beego.NSRouter("/:image_id/layer", &controllers.ImageController{}, "put:PutImageLayer"),
+			beego.NSRouter("/:image_id/checksum", &controllers.ImageController{}, "put:PutChecksum"),
+		),
+	)
 
-  beego.AddNamespace(ns)
+	beego.AddNamespace(ns)
 }
