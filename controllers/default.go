265bd8d5d99c0d7ebb43f77564372d02ef5eec05
Meaglith Ma <genedna@gmail.com>
2014-07-07 16:17:46 +0800

    Add sponsor segment.

--- controllers/default.go
+++ controllers/default.go
@@ -15,7 +15,7 @@ func (this *MainController) Get() {
 	this.Layout = "default.html"
 	this.TplNames = "index.html"
 	this.LayoutSections = make(map[string]string)
-	this.LayoutSections["HtmlHead"] = "index/head.html"
+	this.LayoutSections["Head"] = "index/head.html"
 	this.LayoutSections["Header"] = "header.html"
 	this.LayoutSections["Footer"] = "footer.html"
 	this.Data["Title"] = "Hub"
