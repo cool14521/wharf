e7f36a0c5dad7ffaeea5781bad54777f5c84c447
Meaglith Ma <genedna@gmail.com>
2014-09-08 22:46:24 +0800

    删除七牛备份的 Key 部分

--- cmd/web.go
+++ cmd/web.go
@@ -7,10 +7,8 @@ import (
 	"github.com/astaxie/beego"
 	_ "github.com/astaxie/beego/session/redis"
 	"github.com/codegangsta/cli"
-	"github.com/dockercn/docker-bucket/backup"
 	"github.com/dockercn/docker-bucket/models"
 	_ "github.com/dockercn/docker-bucket/routers"
-	. "github.com/qiniu/api/conf"
 )
 
 var CmdWeb = cli.Command{
@@ -29,16 +27,6 @@ var CmdWeb = cli.Command{
 			Value: 9911,
 			Usage: "Web 服务坚挺的端口，默认 9911",
 		},
-		cli.StringFlag{
-			Name:  "qiniu_access",
-			Value: "",
-			Usage: "七牛云存储服务 Access Key",
-		},
-		cli.StringFlag{
-			Name:  "qiniu_secret",
-			Value: "",
-			Usage: "七牛云存储服务 Secret Key",
-		},
 	},
 }
 
@@ -55,21 +43,6 @@ func runRegistry(c *cli.Context) {
 
 	if len(c.String("port")) > 0 {
 		port = strconv.Itoa(c.Int("port"))
-	}
-
-	if len(c.String("qiniu_access")) > 0 {
-		ACCESS_KEY = c.String("qiniu_access")
-	}
-
-	if len(c.String("qiniu_secret")) > 0 {
-		SECRET_KEY = c.String("qiniu_secret")
-	}
-
-	if len(c.String("qiniu_access")) > 0 && len(c.String("qiniu_secret")) > 0 {
-		backup.Backup = true
-
-		go backup.QiniuBackup(backup.UploadChan, backup.ResultChan)
-		go backup.QiniuResult(backup.ResultChan)
 	}
 
 	beego.SessionProvider = "redis"
