1d3789552956d867b7201ef269a153c2f1970c01
Meaglith Ma <genedna@gmail.com>
2014-11-03 19:17:24 +0800

    增加自定义的 email 验证

--- static/javascript/auth/auth.js
+++ static/javascript/auth/auth.js
@@ -11,29 +11,78 @@ angular.module('auth', ['ngRoute'])
 .controller('SigninCtrl', ['$scope', function ($scope) {
   $scope.login = function (user) {
     console.log(user)
-    // body...
+  // body...
   }
 }])
 .controller('SignupCtrl', ['$scope', function ($scope) {
-  
+
 }])
 .controller('ForgotCtrl', ['$scope', function ($scope) {
-  
+
 }])
 //routes
 .config(function($routeProvider, $locationProvider){
   $routeProvider
-    .when('/', {
-      templateUrl: 'static/views/auth/signin.html',
-      controller: 'SigninCtrl'
-    })
-    .when('/signup', {
-      templateUrl: 'static/views/auth/signup.html',
-      controller: 'SignupCtrl'
-    })
-    .when('/forgot', {
-      templateUrl: 'static/views/auth/forgot.html',
-      controller: 'ForgotCtrl'
-    });
+  .when('/', {
+    templateUrl: 'static/views/auth/signin.html',
+    controller: 'SigninCtrl'
+  })
+  .when('/signup', {
+    templateUrl: 'static/views/auth/signup.html',
+    controller: 'SignupCtrl'
+  })
+  .when('/forgot', {
+    templateUrl: 'static/views/auth/forgot.html',
+    controller: 'ForgotCtrl'
+  });
+})
+//directive
+.directive('passwordCharactersValidator', function() {
 
+  var REQUIRED_PATTERNS = [
+    /\d+/,    //numeric values
+    /[a-z]+/, //lowercase values
+    /[A-Z]+/, //uppercase values
+    /\W+/,    //special characters
+    /^\S+$/   //no whitespace allowed
+  ];
+
+  return {
+    require : 'ngModel',
+    link : function($scope, element, attrs, ngModel) {
+      ngModel.$validators.passwordCharacters = function(value) {
+        var status = true;
+        angular.forEach(REQUIRED_PATTERNS, function(pattern) {
+          status = status && pattern.test(value);
+        });
+
+        return status;
+      }; 
+    }
+  }
+})
+.directive('usernameAvailableValidator', ['$http', function($http) {
+  return {
+    require : 'ngModel',
+    link : function($scope, element, attrs, ngModel) {
+      ngModel.$asyncValidators.usernameAvailable = function(username) {
+        return $http.get('/api/username-exists?u='+ username);
+      };
+    }
+  }
+}])
+.directive('compareToValidator', function() {
+  return {
+    require : 'ngModel',
+    link : function(scope, element, attrs, ngModel) {
+      scope.$watch(attrs.compareToValidator, function() {
+        ngModel.$validate();
+      });
+
+      ngModel.$validators.compareTo = function(value) {
+        var other = scope.$eval(attrs.compareToValidator);
+        return !value || !other || value == other;
+      }
+    }
+  }
 });
