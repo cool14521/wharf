e48b41de8ee2359801a7c396568697586846b88a
Meaglith Ma <genedna@gmail.com>
2014-11-20 01:24:22 +0800

    完成 setting 页面的 header 部分

--- static/javascript/setting/setting.js
+++ static/javascript/setting/setting.js
@@ -8,4 +8,17 @@
 'use strict';
 
 //Auth Page Module
-angular.module('setting', ['ngRoute', 'ngMessages', 'ngCookies', 'angular-growl']);
+angular.module('setting', ['ngRoute', 'ngMessages', 'ngCookies', 'angular-growl'])
+.directive('namespaceValidator', [function (){
+  var NAMESPACE_REGEXP = /^([a-z0-9_]{6,30})$/;
+
+  return {
+    require: 'ngModel',
+    restrict: '',
+    link: function(scope, element, attrs, ngModel) {
+      ngModel.$validators.usernames = function(value) {
+        return USERNAME_REGEXP.test(value);
+      }
+    }
+  };
+}]);
