cbb723141e6c237fbb69b467ab43418057703531
Meaglith Ma <genedna@gmail.com>
2014-11-19 18:51:09 +0800

    初始化 setting 界面

---
+++ controllers/dashboard.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"fmt"
+
+	"github.com/astaxie/beego"
+)
+
+type DashboardController struct {
+	beego.Controller
+}
+
+func (this *DashboardController) Prepare() {
+	beego.Debug(fmt.Sprintf("[%s] %s | %s", this.Ctx.Input.Host(), this.Ctx.Input.Request.Method, this.Ctx.Input.Request.RequestURI))
+
+	beego.Debug("[Header] ")
+	beego.Debug(this.Ctx.Request.Header)
+}
+
+func (this *DashboardController) GetSetting() {
+	this.TplNames = "setting.html"
+
+	this.Data["description"] = ""
+	this.Data["author"] = ""
+
+	this.Render()
+}
---
+++ static/javascript/setting/setting.js
@@ -0,0 +1,11 @@
+/*
+ *  Document   : setting.js
+ *  Author     : Meaglith Ma <genedna@gmail.com> @genedna
+ *  Description: 
+ *
+ */
+
+'use strict';
+
+//Auth Page Module
+angular.module('setting', ['ngRoute', 'ngMessages', 'ngCookies', 'angular-growl']);
