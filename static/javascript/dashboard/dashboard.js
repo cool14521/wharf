677faaa4c9e0caf4ee5eb201b67e8c31baffed8e
Meaglith Ma <genedna@gmail.com>
2014-11-24 03:04:50 +0000

    添加dashboard的css和js文件

---
+++ static/javascript/dashboard/dashboard.js
@@ -0,0 +1,24 @@
+/*
+ *  Document   : setting.js
+ *  Author     : Meaglith Ma <genedna@gmail.com> @genedna
+ *  Description: 
+ *
+ */
+
+'use strict';
+
+//Auth Page Module
+angular.module('dashboard', ['ngRoute', 'ngMessages', 'ngCookies', 'angular-growl'])
+.directive('namespaceValidator', [function (){
+  var NAMESPACE_REGEXP = /^([a-z0-9_]{6,30})$/;
+
+  return {
+    require: 'ngModel',
+    restrict: '',
+    link: function(scope, element, attrs, ngModel) {
+      ngModel.$validators.usernames = function(value) {
+        return USERNAME_REGEXP.test(value);
+      }
+    }
+  };
+}]);
