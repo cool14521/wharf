d0d701ee9f848892b6e3a0012bed26da48c0def5
Chen Liang <chen.liang@crenolab.com>
2014-12-18 18:43:42 +0800

    对模板进行赋值操作

--- controllers/article.go
+++ controllers/article.go
@@ -7,6 +7,7 @@ package controllers
 import (
 	"github.com/astaxie/beego"
 	"github.com/dockercn/docker-bucket/markdown"
+	"strings"
 )
 
 type ArticleController struct {
@@ -16,11 +17,17 @@ type ArticleController struct {
 func (this *ArticleController) GetArticle() {
 	//加载markdown文件
 	category := new(markdown.Category)
-	docs, err := category.Query(false, "private-docker-registry-with-nginx")
+	docs, err := category.Query(false, "automate-docker-and-enhance-your-devops-flows-with-hp-operations-orchestration-en")
 	if err != nil {
 		beego.Trace(err)
 	}
 	this.TplNames = "article.html"
 	this.Data["content"] = docs[0].Content
+	this.Data["title"] = docs[0].Title
+	this.Data["desc"] = docs[0].Desc
+	this.Data["author"] = docs[0].Author
+	this.Data["tags"] = strings.Split(docs[0].Tags, ",")
+	this.Data["views"] = docs[0].Views
+	this.Data["updated"] = docs[0].Updated
 	this.Render()
 }
