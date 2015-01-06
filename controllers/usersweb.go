87cdba333ee4ab3e2bd32abd9ad68f9fcb9947a0
Chen Liang <chen.liang@crenolab.com>
2015-01-07 01:14:50 +0800

    修改处理用户设置头像上传的逻辑

--- controllers/usersweb.go
+++ controllers/usersweb.go
@@ -2,11 +2,10 @@ package controllers
 
 import (
 	"fmt"
+	"github.com/astaxie/beego"
 	"io"
 	"net/http"
 	"os"
-
-	"github.com/astaxie/beego"
 )
 
 type UsersWebController struct {
@@ -28,17 +27,18 @@ func (this *UsersWebController) PostGravatar() {
 		this.ServeJson()
 		return
 	}
-	f, err := os.OpenFile(fileHeader.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	f, err := os.OpenFile(beego.AppConfig.String("docker::Gravatar")+fileHeader.Filename, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		//处理文件错误
 		fmt.Println("OpenFile Error")
 	}
 	defer f.Close()
 	io.Copy(f, file)
+
 	this.Ctx.Output.Context.Output.SetStatus(http.StatusOK)
 	this.Ctx.Output.Context.ResponseWriter.Header().Set("Content-Type", "application/json;charset=UTF-8")
 
-	result = Result{Success: true}
+	result = Result{Success: true, Message: "上传成功", Url: beego.AppConfig.String("docker::Gravatar") + fileHeader.Filename}
 	this.Data["json"] = &result
 	this.ServeJson()
 }
