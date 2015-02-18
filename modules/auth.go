229963ae0d992451d558805f9e9ef92a39d7927f
Meaglith Ma <genedna@gmail.com>
2015-02-18 21:13:03 +0800

    重构 auth.go 代码

--- modules/auth.go
+++ modules/auth.go
@@ -1,7 +1,6 @@
 package modules
 
 import (
-	"fmt"
 	"net/http"
 	"regexp"
 	"strings"
@@ -14,161 +13,93 @@ import (
 )
 
 func authBasic(Ctx *context.Context) (IsAuth bool, ErrCode int, ErrInfo []byte) {
-
 	if len(Ctx.Input.Header("Authorization")) == 0 {
-		//不存在 Authorization 信息返回错误信息
-		beego.Error("[API 用户] Docker 命令访问 HTTP API 的 Header 中没有 Authorization 信息: ")
+		beego.Error("[REGISTRY API V1] Docker HTTP API Without Authorization In Header")
 		beego.Error(Ctx.Request.Header)
-
-		ErrCode = http.StatusUnauthorized
-		ErrInfo = []byte("{\"错误\":\"没有找到 Authorization 的认证信息\"}")
-		IsAuth = false
-		return
+		return false, http.StatusUnauthorized, []byte("Docker HTTP API Without Authorization In Header")
 	}
-	isBasicIndex := strings.Index(Ctx.Input.Header("Authorization"), "Basic")
 
-	if isBasicIndex == -1 {
-		beego.Error("[API 用户] Docker 命令访问 HTTP API 的 Header 中没有 Basic Auth 和 Token 的信息 ")
+	if strings.Index(Ctx.Input.Header("Authorization"), "Basic") == -1 {
+		beego.Error("[REGISTRY API V1] Docker HTTP API Without Authorization And Token In Header")
 		beego.Error(Ctx.Request.Header)
-
-		ErrCode = http.StatusUnauthorized
-		ErrInfo = []byte("{\"错误\":\"没有找到 Authorization 的认证信息\"}")
-		IsAuth = false
-		return
-
-	}
-	//解码 Basic Auth 进行用户的判断
-	username, passwd, err := utils.DecodeBasicAuth(Ctx.Input.Header("Authorization"))
-
-	if err != nil {
-		beego.Error(fmt.Sprintf("[API 用户] 解码 Header Authorization 的 Basic Auth [%s] 时遇到错误： %s", Ctx.Input.Header("Authorization"), err.Error()))
-		ErrCode = http.StatusUnauthorized
-		ErrInfo = []byte([]byte("{\"错误\":\"解码 Authorization 的 Basic Auth 信息错误\"}"))
-		IsAuth = false
-		return
-
+		return false, http.StatusUnauthorized, []byte("Docker HTTP API Without Authorization And Token In Header")
 	}
 
-	//根据解码的数据，在数据库中查询用户
-	user := new(models.User)
-	err = user.Get(username, passwd)
-	if err != nil {
-		//查询用户数据失败，返回 401 错误
-		beego.Error(fmt.Sprintf("[API 用户] 验证用户错误：%s", err.Error()))
-		ErrCode = http.StatusUnauthorized
-		ErrInfo = []byte(`{"错误":"验证用户错误"}`)
-		IsAuth = false
-		return
+	if username, passwd, err := utils.DecodeBasicAuth(Ctx.Input.Header("Authorization")); err != nil {
+		beego.Error("[REGISTRY API V1] Decode Header Authorization Basic Auth Error: ", err.Error())
+		return false, http.StatusUnauthorized, []byte("Decode Header Authorization Basic Auth Error")
+	} else {
+		user := new(models.User)
+		if err = user.Get(username, passwd); err != nil {
+			beego.Error("[REGISTRY API V1] User Authorization Error: ", err.Error())
+
+			return false, http.StatusUnauthorized, []byte("User Authorization Error")
+		}
+
+		return true, 0, nil
 	}
-
-	IsAuth = true
-	ErrCode = 0
-	ErrInfo = nil
-	return
-
 }
 
-func authNamespace(Ctx *context.Context) (IsAuth bool, NamespaceType bool, ErrCode int, ErrInfo []byte, Read bool, Write bool) {
-
-	//根据 Namespace 查询数据
+func authNamespace(Ctx *context.Context) (Auth bool, NamespaceType bool, Code int, Message []byte, Read bool, Write bool) {
 	namespace := string(Ctx.Input.Param(":namespace"))
 	repository := string(Ctx.Input.Param(":repository"))
-	//	NamespaceType = false //默认用户为普通户用，不是组织
-
-	//判断是用户还是组织
 
 	org := new(models.Organization)
-	orgIsHas, _, err := org.Has(namespace)
+	isOrg, _, err := org.Has(namespace)
 	if err != nil {
-		beego.Error(fmt.Sprintf("[API 用户] 查询组织 %s 时错误 %s", namespace, err.Error()))
-		ErrCode = http.StatusForbidden
-		ErrInfo = []byte(`{"错误":"查询组织报错。"}`)
-		IsAuth = false
-		return
+		beego.Error("[REGISTRY API V1] Search Organization Error: ", err.Error())
+		return false, false, http.StatusForbidden, []byte("Search Organization Error"), false, false
 	}
 
 	user := new(models.User)
-	userIsHas, _, err := user.Has(namespace)
+	isUser, _, err := user.Has(namespace)
 	if err != nil {
-		beego.Error(fmt.Sprintf("[API 用户] 查询用户 %s 时错误 %s", namespace, err.Error()))
-		ErrCode = http.StatusForbidden
-		ErrInfo = []byte(`{"错误":"查询用户报错。"}`)
-		IsAuth = false
-		return
+		beego.Error("[REGISTRY API V1] Search User Error: ", err.Error())
+		return false, false, http.StatusForbidden, []byte("Search User Error"), false, false
 	}
 
-	if !orgIsHas && !userIsHas {
-		beego.Error(fmt.Sprintf("[API 用户] 没有查询到namespace: %s", namespace))
-		ErrCode = http.StatusForbidden
-		ErrInfo = []byte(`{"错误":"没有查询到namespace。"}`)
-		IsAuth = false
-		return
+	if !isUser && !isOrg {
+		beego.Error("[REGISTRY API V1] Search Namespace Error")
+		return false, false, http.StatusForbidden, []byte("Search Namespace Error"), false, false
 	}
 
-	ErrCode = 0
-	ErrInfo = nil
-	IsAuth = true
-	Read = false
-	Write = false
-	NamespaceType = false //默认用户为普通户用，不是组织
+	Auth, NamespaceType, Code, Message, Read, Write = false, false, 0, nil, false, false
 
-	if orgIsHas {
-		//NamespaceType = true
-		//判断用户所在NameSpace指定的组织
-		//读取用户所有的组织
-		for index, value := range user.Organizations {
-			fmt.Printf("遍历第[%d]个切片\n", index)
-			//查找
+	if isOrg == true {
+
+		for _, value := range user.Organizations {
 			organization := new(models.Organization)
-			err = organization.Get(value)
-			if err != nil {
-				beego.Error(fmt.Sprintf("[API 用户] 验证用户所在组织namespace错误: %s", namespace))
-				ErrCode = http.StatusForbidden
-				ErrInfo = []byte(`{"错误":"验证用户所在组织namespace错误"}`)
-				IsAuth = false
-				return
+
+			if err := organization.Get(value); err != nil {
+				beego.Error("[REGISTRY API V1] Search Organization Error")
+				return false, false, http.StatusForbidden, []byte("Search Organization Error"), false, false
 			}
+
 			if namespace == organization.Organization {
-				//用户在namespace 指定的组织
 				NamespaceType = true
 			}
 		}
 
-		//用户不在指定的组织
-		if !NamespaceType {
-			beego.Error(fmt.Sprintf("[API 用户] 用户不在指定的组织: %s", namespace))
-			ErrCode = http.StatusForbidden
-			ErrInfo = []byte(`{"错误":"用户不在指定的组织"}`)
-			IsAuth = false
-			return
+		if NamespaceType == false {
+			return false, false, http.StatusForbidden, []byte("User not in the organization"), false, false
 		}
 
-		//判断用户所有Team 对于此仓库权限的集合
-		for index, value := range user.Teams {
-			fmt.Printf("遍历第[%d]个Team\n", index)
+		for _, value := range user.Teams {
 			team := new(models.Team)
-			err = team.Get(value)
-			if err != nil {
-				beego.Error(fmt.Sprintf("[API 用户] 验证用户所在Team错误"))
-				ErrCode = http.StatusForbidden
-				ErrInfo = []byte(`{"错误":"验证用户所在Team错误"}`)
-				IsAuth = false
-				return
-			}
-			for Pindex, Pvalue := range team.TeamPrivileges {
-				fmt.Printf("遍历第[%d]个TeamPrivileges\n", Pindex)
-				privilege := new(models.Privilege)
-				err = privilege.Get(Pvalue)
 
-				if err != nil {
-					beego.Error(fmt.Sprintf("[API 用户] 验证用户所在Team对应的仓库权限错误"))
-					ErrCode = http.StatusForbidden
-					ErrInfo = []byte(`{"错误":"验证用户所在Team对应的仓库权限错误"}`)
-					IsAuth = false
-					return
+			if err := team.Get(value); err != nil {
+				beego.Error("[REGISTRY API V1] Search Team Error")
+				return false, false, http.StatusForbidden, []byte("Search Team Error"), false, false
+			}
+
+			for _, v := range team.TeamPrivileges {
+				privilege := new(models.Privilege)
+				if err := privilege.Get(v); err != nil {
+					return false, false, http.StatusForbidden, []byte("Search Team Privilege Error"), false, false
 				}
+
 				if privilege.Repository == repository {
-					if privilege.Privilege {
+					if privilege.Privilege == true {
 						Read = true
 						Write = true
 					} else if Read == false {
@@ -176,58 +107,37 @@ func authNamespace(Ctx *context.Context) (IsAuth bool, NamespaceType bool, ErrCo
 						Write = false
 					}
 
+					return true, NamespaceType, Code, Message, Read, Write
 				}
-
 			}
-
 		}
-
 	} else {
-		ErrCode = 0
-		ErrInfo = nil
-		IsAuth = true
-		Read = true
-		Write = true
-		NamespaceType = false //默认用户为普通户用，不是组织
+		if user.Username == namespace {
+			Auth = true
+		} else {
+			Auth, Code, Message = false, http.StatusUnauthorized, []byte("Unauthorized Namespace")
+		}
 	}
 
-	return
-
+	return Auth, NamespaceType, Code, Message, Read, Write
 }
 
-func authToken(Ctx *context.Context) (IsAuth bool, ErrCode int, ErrInfo []byte) {
-
-	//非 Basic Auth ，检查 Token
+func authToken(Ctx *context.Context) (bool, int, []byte) {
 	if strings.Index(Ctx.Input.Header("Authorization"), "Token") == -1 {
-		beego.Error("[API 用户] Docker 命令访问 HTTP API 的 Header 中没有 Basic Auth 和 Token 的信息 ")
-		beego.Error(Ctx.Request.Header)
-		IsAuth = false
-		ErrCode = http.StatusUnauthorized
-		ErrInfo = []byte("{\"错误\":\"在 HTTP Header Authorization 中没有找到 Basic Auth 和 Token 信息\"}")
-		return
+		return false, http.StatusUnauthorized, []byte("No Basic Auth And Token In HTTP Header Authorization")
 	}
 
-	//使用正则获取 Token 的值
 	r, _ := regexp.Compile(`Token (?P<token>\w+)`)
 	tokens := r.FindStringSubmatch(Ctx.Input.Header("Authorization"))
 	_, token := tokens[0], tokens[1]
 
-	beego.Debug("[Token in Header] " + token)
-
 	t := Ctx.Input.Session("token")
 
-	//用 Header 中的 Token 和 Session 中得 Token 值进行比较，不相等返回错误退出执行
 	if token != t {
-		IsAuth = false
-		ErrCode = http.StatusUnauthorized
-		ErrInfo = []byte("{\"错误\":\"HTTP Header 中的 Token 和 Session 的 Token 不同\"}")
-		return
+		return false, http.StatusUnauthorized, []byte("Unauthorized Token")
 	}
 
-	IsAuth = true
-	ErrCode = 0
-	ErrInfo = nil
-	return
+	return true, 0, []byte("")
 }
 
 func AuthPutRepository(Ctx *context.Context) (bool, int, []byte) {
