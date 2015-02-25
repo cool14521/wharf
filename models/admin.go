f7c2e145b152de50bc0a68ee7372fae4a97d76fc
Meaglith Ma <genedna@gmail.com>
2015-02-25 23:01:53 +0800

    Remove the Chinese comments.

--- models/admin.go
+++ models/admin.go
@@ -1,11 +1,11 @@
 package models
 
 type Admin struct {
-  UUID     string `json:"UUID"`     //全局唯一的索引, LedisDB Admin List 保存全局所有的 UUID 列表信息，LedisDB独立保存每个用户信息到一个 hash,名字为 {UUID}
-  Username string `json:"username"` //用于保存用户的登录名,全局唯一
-  Password string `json:"password"` //保存系统管理员 MD5 后的密码
-  Email    string `json:"email"`    //
-  Created  int64  `json:"created"`  //系统管理员创建时间
-  Updated  int64  `json:"updated"`  //系统管理员信息更新时间
-  Memo     string `json:"memo"`     //
+	UUID     string `json:"UUID"`     //
+	Username string `json:"username"` //
+	Password string `json:"password"` //
+	Email    string `json:"email"`    //
+	Created  int64  `json:"created"`  //
+	Updated  int64  `json:"updated"`  //
+	Memo     string `json:"memo"`     //
 }
--- models/user.go
+++ models/user.go
@@ -1,123 +1,123 @@
 package models
 
 import (
-  "fmt"
-  "regexp"
+	"fmt"
+	"regexp"
 )
 
 type User struct {
-  UUID          string   `json:"UUID"`          //全局唯一的索引, LedisDB User List 保存全局所有的用户 UUID 列表信息，LedisDB 独立保存每个用户信息到一个 hash,名字为 {UUID}
-  Username      string   `json:"username"`      //用于保存用户的登录名,全局唯一
-  Password      string   `json:"password"`      //保存用户的密码
-  Email         string   `json:"email"`         //
-  Fullname      string   `json:"fullname"`      //保存用户全名
-  Company       string   `json:"company"`       //用户所属公司
-  Location      string   `json:"location"`      //用户所在地
-  Mobile        string   `json:"mobile"`        //用户电话
-  URL           string   `json:"url"`           //用户主页URL
-  Gravatar      string   `json:"gravatar"`      //用户头像地址 如果是邮件地址，使用 gravatar.org 进行解析
-  Created       int64    `json:"created"`       //用户创建时间
-  Updated       int64    `json:"updated"`       //用户信息更新时间
-  Repositories  []string `json:"repositories"`  //用户具备所有权的 respository 对应 UUID 信息,最新添加的 Repository 在最前面
-  Organizations []string `json:"organizations"` //用户所有的组织对应 UUID 信息，最新添加的在最前面
-  Teams         []string `json:"teams"`         //用户所有的 Team 对应 UUID 信息，最新添加的在最前面
-  Starts        []string `json:"starts"`        //用户加星的 Respository 对应 UUID 信息,最新添加的在最前面
-  Comments      []string `json:"comments"`      //和用户相关的所有评论对应 UUID 信息，包括自己发的评论和别人评论相关自己的，最新的评论在最前面
-  Memo          string   `json:"memo"`          //
+	UUID          string   `json:"UUID"`          //
+	Username      string   `json:"username"`      //
+	Password      string   `json:"password"`      //
+	Email         string   `json:"email"`         //
+	Fullname      string   `json:"fullname"`      //
+	Company       string   `json:"company"`       //
+	Location      string   `json:"location"`      //
+	Mobile        string   `json:"mobile"`        //
+	URL           string   `json:"url"`           //
+	Gravatar      string   `json:"gravatar"`      //
+	Created       int64    `json:"created"`       //
+	Updated       int64    `json:"updated"`       //
+	Repositories  []string `json:"repositories"`  //
+	Organizations []string `json:"organizations"` //
+	Teams         []string `json:"teams"`         //
+	Starts        []string `json:"starts"`        //
+	Comments      []string `json:"comments"`      //
+	Memo          string   `json:"memo"`          //
 }
 
 func (user *User) Has(username string) (bool, []byte, error) {
 
-  UUID, err := GetUUID("user", username)
+	UUID, err := GetUUID("user", username)
 
-  if err != nil {
-    return false, nil, err
-  }
+	if err != nil {
+		return false, nil, err
+	}
 
-  if len(UUID) <= 0 {
-    return false, nil, nil
-  }
+	if len(UUID) <= 0 {
+		return false, nil, nil
+	}
 
-  err = Get(user, UUID)
+	err = Get(user, UUID)
 
-  return true, UUID, err
+	return true, UUID, err
 }
 
 func (user *User) Save() error {
-  //https://github.com/docker/docker/blob/28f09f06326848f4117baf633ec9fc542108f051/registry/registry.go#L27
-  validNamespace := regexp.MustCompile(`^([a-z0-9_]{4,30})$`)
-  if !validNamespace.MatchString(user.Username) {
-    return fmt.Errorf("Username must be 4 - 30, include a-z, 0-9 and '_'")
-  }
+	//https://github.com/docker/docker/blob/28f09f06326848f4117baf633ec9fc542108f051/registry/registry.go#L27
+	validNamespace := regexp.MustCompile(`^([a-z0-9_]{4,30})$`)
+	if !validNamespace.MatchString(user.Username) {
+		return fmt.Errorf("Username must be 4 - 30, include a-z, 0-9 and '_'")
+	}
 
-  if len(user.Password) < 5 {
-    return fmt.Errorf("Password length should be more than 5")
-  }
+	if len(user.Password) < 5 {
+		return fmt.Errorf("Password length should be more than 5")
+	}
 
-  validEmail := regexp.MustCompile("[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[a-zA-Z0-9](?:[\\w-]*[\\w])?")
-  if !validEmail.MatchString(user.Email) {
-    return fmt.Errorf("Email illegal")
-  }
+	validEmail := regexp.MustCompile("[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[a-zA-Z0-9](?:[\\w-]*[\\w])?")
+	if !validEmail.MatchString(user.Email) {
+		return fmt.Errorf("Email illegal")
+	}
 
-  if err := Save(user, []byte(user.UUID)); err != nil {
-    return err
-  }
+	if err := Save(user, []byte(user.UUID)); err != nil {
+		return err
+	}
 
-  if _, err := LedisDB.HSet([]byte(GLOBAL_USER_INDEX), []byte(user.Username), []byte(user.UUID)); err != nil {
-    return err
-  }
+	if _, err := LedisDB.HSet([]byte(GLOBAL_USER_INDEX), []byte(user.Username), []byte(user.UUID)); err != nil {
+		return err
+	}
 
-  return nil
+	return nil
 }
 
 func (user *User) Remove() error {
-  if _, err := LedisDB.HSet([]byte(fmt.Sprintf("%s_remove", GLOBAL_USER_INDEX)), []byte(user.Username), []byte(user.UUID)); err != nil {
-    return err
-  }
+	if _, err := LedisDB.HSet([]byte(fmt.Sprintf("%s_remove", GLOBAL_USER_INDEX)), []byte(user.Username), []byte(user.UUID)); err != nil {
+		return err
+	}
 
-  if _, err := LedisDB.HDel([]byte(GLOBAL_USER_INDEX), []byte(user.Username)); err != nil {
-    return err
-  }
+	if _, err := LedisDB.HDel([]byte(GLOBAL_USER_INDEX), []byte(user.Username)); err != nil {
+		return err
+	}
 
-  return nil
+	return nil
 }
 
 func (user *User) Get(username, password string) error {
-  if exist, UUID, err := user.Has(username); err != nil {
-    return err
-  } else if exist == false && err == nil {
-    return fmt.Errorf("User is not exist: %s", username)
-  } else if exist == true && err == nil {
-    if err := Get(user, UUID); err != nil {
-      return err
-    } else {
-      if user.Password != password {
-        return fmt.Errorf("User password error.")
-      } else {
-        return nil
-      }
-    }
-  }
+	if exist, UUID, err := user.Has(username); err != nil {
+		return err
+	} else if exist == false && err == nil {
+		return fmt.Errorf("User is not exist: %s", username)
+	} else if exist == true && err == nil {
+		if err := Get(user, UUID); err != nil {
+			return err
+		} else {
+			if user.Password != password {
+				return fmt.Errorf("User password error.")
+			} else {
+				return nil
+			}
+		}
+	}
 
-  return nil
+	return nil
 }
 
 func (user *User) Orgs(username string) (map[string]string, error) {
-  result := map[string]string{}
+	result := map[string]string{}
 
-  if exist, _, err := user.Has(username); err != nil {
-    return nil, err
-  } else if exist == false && err == nil {
-    return nil, fmt.Errorf("User is not exist: %s", username)
-  } else if exist == true && err == nil {
-    for _, uuid := range user.Organizations {
-      var org Organization
+	if exist, _, err := user.Has(username); err != nil {
+		return nil, err
+	} else if exist == false && err == nil {
+		return nil, fmt.Errorf("User is not exist: %s", username)
+	} else if exist == true && err == nil {
+		for _, uuid := range user.Organizations {
+			var org Organization
 
-      if err := org.Get(uuid); err == nil {
-        result[org.Organization] = org.UUID
-      }
-    }
-  }
+			if err := org.Get(uuid); err == nil {
+				result[org.Organization] = org.UUID
+			}
+		}
+	}
 
-  return result, nil
+	return result, nil
 }
