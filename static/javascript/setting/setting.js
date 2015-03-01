fb73d767ab7c3635c576e97d3dbe703288dce7df
Chen Liang <chen.liang@crenolab.com>
2015-03-02 00:00:57 +0800

    增加dashboard页面的仓库展示

--- static/javascript/setting/setting.js
+++ static/javascript/setting/setting.js
@@ -9,6 +9,62 @@
 
 //Auth Page Module
 angular.module('setting', ['ngRoute', 'ngMessages', 'ngCookies', 'angular-growl', 'angularFileUpload'])
+    .controller('AddRepositoryCtrl', ['$scope', '$cookies', '$http', 'growl', '$location', '$timeout', '$window', function($scope, $cookies, $http, growl, $location, $timeout, $window) {
+        $scope.privated = {};
+        $scope.namespaces = {};
+        $scope.repository = {};
+        $scope.namespaceObject = {};
+
+        //init user data
+        $scope.addPrivilege = false
+        $http.get('/w1/namespaces')
+            .success(function(data, status, headers, config) {
+                $scope.namespaces = data;
+                /* $scope.repository.namespace = data[0];*/
+                $scope.namespaceObject = data[0];
+            })
+            .error(function(data, status, headers, config) {
+
+            });
+        $scope.privated = {};
+        $scope.privated.values = [{
+            code: 0,
+            name: "Public"
+        }, {
+            code: 1,
+            name: "Private"
+        }];
+
+        $scope.privated.selection = $scope.privated.values[0];
+
+        //deal with create repository
+        $scope.createRepo = function() {
+            if ($scope.repoCreateForm.$valid) {
+                if ($scope.privated.selection.code == 1) {
+                    $scope.repository.privated = true;
+                } else {
+                    $scope.repository.privated = false;
+                }
+
+                $scope.repository.namespace = $scope.namespaceObject.namespace;
+                $scope.repository.namespacetype = $scope.namespaceObject.namespacetype;
+
+                $http.defaults.headers.post['X-XSRFToken'] = base64_decode($cookies._xsrf.split('|')[0]);
+                $http.post('/w1/repository', $scope.repository)
+                    .success(function(data, status, headers, config) {
+                        $scope.addPrivilege = true;
+                        growl.info(data.message);
+                        $timeout(function() {
+                            $window.location.href = '/dashboard';
+                        }, 3000);
+                    })
+                    .error(function(data, status, headers, config) {
+                        growl.error(data.message);
+                    });
+            }
+        }
+
+    }])
     .controller('SettingProfileCtrl', ['$scope', '$cookies', '$http', 'growl', '$location', '$timeout', '$upload', function($scope, $cookies, $http, growl, $location, $timeout, $upload) {
         //init user info
         $http.get('/w1/profile')
@@ -194,7 +250,7 @@ angular.module('setting', ['ngRoute', 'ngMessages', 'ngCookies', 'angular-growl'
         $("#tags").autocomplete({
             source: availableTags
         });
-        
+
         $scope.addUserFunc = function() {
             $scope.findUser = {}
             $scope.findUser.username = document.getElementById("tags").value;
@@ -426,6 +482,10 @@ angular.module('setting', ['ngRoute', 'ngMessages', 'ngCookies', 'angular-growl'
             .when('/notification', {
                 templateUrl: '/static/views/setting/notification.html',
                 controller: 'SettingNotificationCtrl'
+            })
+            .when('/repo/add', {
+                templateUrl: '/static/views/dashboard/repositoryadd.html',
+                controller: 'AddRepositoryCtrl'
             })
             .when('/org/add', {
                 templateUrl: '/static/views/setting/organizationadd.html',
